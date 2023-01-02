74fb20fef06d8a9fedefeeda91d56abb81ef5574
chcjsdud 조해근 <chcjsdud@naver.com>
2023-01-02 17:50:25 +0900

    조해근 : 윈도우 업데이트

--- WINAPI/APIApp/APIApp/main.cpp
+++ WINAPI/APIApp/APIApp/main.cpp
@@ -1,11 +1,21 @@
-#include <GameEngineDebug.h>
+#include <Windows.h>
+#include <GameEngineBase/GameEngineDebug.h>
+#include <GameEnginePlatform/GameEngineWindow.h>
 
-int main() 
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-	// GameEngineDebug::LeckCheck();
-	GameEngineDebug::LeckCheck();
 
-	//LeckCheck();
+	GameEngineDebug::LeakCheck();
+
+	GameEngineWindow::WindowCreate(hInstance, "MainWindow");
+
+	GameEngineWindow::WindowLoop();
+
+	// main이 끝나면 무조건 끝나기 때문에
+	// 무조건 붙잡아둬야 합니다.
 
 	return 1;
 }
