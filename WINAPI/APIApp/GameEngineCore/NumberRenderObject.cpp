274da2f836d7b770d4bf92ef516a82b4fbc05dd5
승민 황 <iqmin11@gmail.com>
2023-02-10 13:32:41 +0900

    황승민 : float4 오퍼레이터 추가 및 NumberRenderObject 수정 건의 커밋

--- WINAPI/APIApp/GameEngineCore/NumberRenderObject.cpp
+++ WINAPI/APIApp/GameEngineCore/NumberRenderObject.cpp
@@ -3,11 +3,11 @@
 #include "GameEngineActor.h"
 #include <GameEngineBase/GameEngineString.h>
 
-NumberRenderObject::NumberRenderObject() 
+NumberRenderObject::NumberRenderObject()
 {
 }
 
-NumberRenderObject::~NumberRenderObject() 
+NumberRenderObject::~NumberRenderObject()
 {
 }
 
@@ -25,7 +25,7 @@ void NumberRenderObject::SetImage(const std::string_view& _ImageName, float4 _Sc
 	{
 		MsgAssert("크기가 0으로 숫자를 출력할 수 없습니다.");
 	}
-	
+
 	ImageName = _ImageName;
 	NumberScale = _Scale;
 	Order = _Order;
@@ -59,7 +59,7 @@ void NumberRenderObject::SetValue(int _Value)
 
 	float4 RenderPos;
 
-	Negative = _Value > 0 ? false : true;
+	Negative = _Value >= 0 ? false : true;
 
 	if (true == Negative && nullptr == NegativeRender)
 	{
@@ -101,11 +101,17 @@ void NumberRenderObject::SetValue(int _Value)
 		break;
 	case Align::Right:
 		SetMove(float4::Left * static_cast<const float>(GameEngineMath::GetLenth(Value) - 1) * NumberScale.x);
-		NegativeRender->SetPosition(float4::Left * static_cast<const float>(GameEngineMath::GetLenth(Value) - 1) * NumberScale.x);
+		if (nullptr != NegativeRender)
+		{
+			NegativeRender->SetPosition(Pos + float4::Left * static_cast<const float>(GameEngineMath::GetLenth(Value) - 1) * NumberScale.x);
+		}
 		break;
 	case Align::Center:
 		SetMove((float4::Left * static_cast<const float>(GameEngineMath::GetLenth(Value) - 1) * NumberScale.x).half());
-		NegativeRender->SetPosition((float4::Left * static_cast<const float>(GameEngineMath::GetLenth(Value) - 1) * NumberScale.x).half());
+		if (nullptr != NegativeRender)
+		{
+			NegativeRender->SetPosition(Pos + (float4::Left * static_cast<const float>(GameEngineMath::GetLenth(Value) - 1) * NumberScale.x).half());
+		}
 		break;
 	default:
 		break;
@@ -130,6 +136,50 @@ void NumberRenderObject::SetMove(float4 _RenderPos)
 void NumberRenderObject::SetAlign(Align _Align)
 {
 	AlignState = _Align;
+}
+
+void NumberRenderObject::SetRenderPos(float4 _Pos)
+{
+	Pos = _Pos;
+
+	switch (AlignState)
+	{
+	case Align::Left:
+		for (size_t i = 0; i < NumberRenders.size(); i++)
+		{
+			NumberRenders[i]->SetPosition(_Pos + float4::Right * NumberRenders[i]->GetScale() * static_cast<const float>(i));
+		}
+
+		if (nullptr != NegativeRender)
+		{
+			NegativeRender->SetPosition(_Pos + float4::Left * NumberScale.x);
+		}
+		break;
+	case Align::Right:
+		for (size_t i = 0; i < NumberRenders.size(); i++)
+		{
+			NumberRenders[i]->SetPosition(_Pos + float4::Left * NumberRenders[i]->GetScale() * (static_cast<const float>(NumberRenders.size()) - (static_cast<const float>(i) + 1)));
+		}
+
+		if (nullptr != NegativeRender)
+		{
+			NegativeRender->SetPosition(_Pos + float4::Left * static_cast<const float>(GameEngineMath::GetLenth(Value) - 1) * NumberScale.x);
+		}
+		break;
+	case Align::Center:
+		for (size_t i = 0; i < NumberRenders.size(); i++)
+		{
+			NumberRenders[i]->SetPosition(_Pos + (float4::Left * NumberRenders[i]->GetScale().half() * (static_cast<const float>(NumberRenders.size()) - 1)) + (float4::Right * NumberRenders[i]->GetScale() * static_cast<const float>(i)));
+		}
+
+		if (nullptr != NegativeRender)
+		{
+			NegativeRender->SetPosition(_Pos + (float4::Left * static_cast<const float>(GameEngineMath::GetLenth(Value) - 1) * NumberScale.x).half());
+		}
+		break;
+	default:
+		break;
+	}
 }
 
 
