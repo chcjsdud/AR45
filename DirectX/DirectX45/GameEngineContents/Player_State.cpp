a7e5cc02e1b2eb2579a2a9477544b75c0b409a71
chcjsdud <chcjsdud@naver.com>
2023-05-22 18:18:59 +0900

    조해근 : 충돌체 기본 타입 기능 업데이트

--- DirectX/DirectX45/GameEngineContents/Player_State.cpp
+++ DirectX/DirectX45/GameEngineContents/Player_State.cpp
@@ -135,16 +135,15 @@ void Player::StateInit()
 				GetTransform()->AddLocalScale({ -ScaleSpeed * _DeltaTime, 0.0f, 0.0f });
 			}
 
-			//std::vector<std::shared_ptr<GameEngineCollision>> ColTest;
-
-			//if (Collsion->CollisionAll(2000, ColType::SPHERE2D, ColType::SPHERE2D, ColTest), 0 != ColTest.size())
-			//{
-			//	for (std::shared_ptr<GameEngineCollision> Col : ColTest)
-			//	{
-			//		Col->GetActor()->Death();
-			//	}
-			//	// Col->GetActor()->Death();
-			//}
+			std::vector<std::shared_ptr<GameEngineCollision>> ColTest;
+			if (Collsion->CollisionAll(2000, ColTest), 0 != ColTest.size())
+			{
+				for (std::shared_ptr<GameEngineCollision> Col : ColTest)
+				{
+					Col->GetActor()->Death();
+				}
+				// Col->GetActor()->Death();
+			}
 
 			float4 Pos = GetTransform()->GetLocalPosition();
 
