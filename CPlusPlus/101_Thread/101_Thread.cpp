558284585e573536feae072fc1339d7ba1c493ce
chcjsdud <chcjsdud@naver.com>
2023-06-21 19:35:38 +0900

    조해근 : 쓰레드 기초교육 업데이트

--- CPlusPlus/101_Thread/101_Thread.cpp
+++ CPlusPlus/101_Thread/101_Thread.cpp
@@ -3,6 +3,11 @@
 
 #include <iostream>
 #include <thread>
+#include <atomic> // 운영체제의 함수를 사용하고 있을겁니다.
+				  // 인터락 함수라는걸 쓰고 있다. CPU의 특정 메모리를 사용해서 
+			      // 특정 메모리의 최소한의 락을 걸어주는 기계어수준의 로직이 들어가있는 메모리 관리 클래스
+				  // 어마어마하게 속력이 좋은 락. 8바이트정도
+
 #include <mutex>
 // 락이나 쓰레드나 내부에서 리눅스 윈도우 관계 없이 돌아갈수 있게 만들어졌다.
 // 내부에서 대신 리눅스나 윈도우의 쓰레드 함수를 호출해주고 있다는것을 잊으면 안된다.
@@ -13,15 +18,23 @@
 
 
 // 1. 쓰레드에서 가장 중요한 공유메모리 수정 문제
+//    // 쓰레드는 스택만 서로 다른걸 사용하지
+//    // 코드 데이터 힙은 공유하기 때문에 공유메모리 문제가 생길수밖에 없다.   
+// 
 //    1-0. 공유만 하는건 문제가 안된다. 수정하려고 하면 문제가 생긴다.
 //    1-1. 각자 메모리를 가지면 된다.
-//    1-2. 락을 건다. 
+//	  1-2. 아토믹을 사용한다. 
+//    1-3. 락을 건다. 
 // 
 // 2. 쓰레드를 많이 만든다고 무조건 빨라지는 건 아니다.
 //    2-1. 쓰레드가 많아지면 많아질수록 컨텍스트 스위칭도 늘어난다.
 //    2-2. CPU 졸라많으면 해결되는거 아니야? 
 //    안된다. 전력소비 까지 생각해보면 결국에는 효율이 떨어지게 된다. 암달의 법칙
 // 
+// 3. 대부분의 일반적인 연산에서
+//    누가 먼저 되야해. <= 이런게 있으면 쓰레드를 쓸 이유가 없다.
+
+
 
 std::map<int, int> VVV;
 
@@ -32,7 +45,16 @@ std::map<int, int> VVV;
 // 공유되는 메모리당 1개씩 만듭니다.
 
 int Value = 0;
+// 락도 걸지 않으면 걸지 않을수록 좋다.
 std::mutex ValueLock;
+
+std::atomic<int> AtomicValue = 0;
+
+int Value0 = 0;
+int Value1 = 0;
+
+std::atomic_bool Thread1 = false;
+std::atomic_bool Thread2 = false;
 
 void Test() 
 {
@@ -41,7 +63,10 @@ void Test()
 	// 0
 
 
-	for (size_t i = 0; i < 10000000; i++)
+	// 0.01
+	// 0.10
+	// ValueLock.lock();
+	for (size_t i = 0; i < 100000000; i++)
 	{
 		// Value는 얼마지?
 		
@@ -51,33 +76,47 @@ void Test()
 		// Value에 Value +1한 메모리를 복사한다.
 
 		// 락을 쓴다는건 컨텍스트 스위칭을 일부러 일으키겠다는 이야기가 된다.
-		ValueLock.lock();
-		Value = Value + 1;
-		ValueLock.unlock();
+		// Value0 = Value0 + 1;
+		// AtomicValue = AtomicValue + 1;
+		// 
+		++AtomicValue;
+		// 열심히 텍스처 로딩
 
 		// 자료구조의 함수를 사용하는 언제나 위험할수 있다.
 		// VVV[10]
 		// VVV.operator[](10);
 	}
+	// ValueLock.unlock();
 
+	Thread1 = true;
+
+	// 쓰레드의 끝
 }
+
+
 void Test1()
 {
 
-	for (size_t i = 0; i < 10000000; i++)
+
+		// ValueLock.lock();
+	for (size_t i = 0; i < 100000000; i++)
 	{
 		// Value는 얼마지?
 
 		// Value를 준비한다. 0
 		// Value에 +1을 한 메모리를 만든다. 1
 		// Value에 Value +1한 메모리를 복사한다. 3
-		ValueLock.lock();
 
-		Value = Value + 1;
-		ValueLock.unlock();
+		// Value1 = Value1 + 1;
+
+		// AtomicValue = AtomicValue + 1;
+		// 그나마 대안이 될수 있다.
+		++AtomicValue;
 
 	}
+		// ValueLock.unlock();
 
+	Thread2 = true;
 }
 
 int main()
@@ -91,15 +130,51 @@ int main()
 	// 쓰레드를 가장 잘 이용하는 법.
 	// 필요할때만 쓰레드를 깨우는 이용법이 최고의 이용법입니다.
 	std::thread NewThread0 = std::thread(Test);
+	// 일반적인 함수의 실행은 이 함수가 끝나기 전까지는 Test
+	// 동기함수
+
+	// Test();
+
+	// Test가 끝날때까지 기다린다.
+	// 그런데 기다리면 안된다.
+	// NewThread0.join();
+	
+	// 이런걸 동기 함수라고 한다.
+	// _getch();
+
+
 	std::thread NewThread1 = std::thread(Test1);
+
 
 	// 리눅스에서는 
 	// 내부에서 _beginthreadex를 사용하는 모습을 볼수가 있다.
 
+
+
+	// std::cout << Value;
+
+	while (true)
+	{
+		// 게임의 프레임
+		// std::cout << Value0 + Value1;
+
+		std::cout << "게임은 계속 되고 있다 프레임" << std::endl;
+		
+		// 이게 쓰레드를 사용하는 가장 이상적인 기본형
+		// 텍스처 로딩 끝났나?
+		if (true == Thread1 && true == Thread2)
+		{
+			// LevelChange()
+			// std::cout << Value0 + Value1 << std::endl;
+
+			std::cout << AtomicValue << std::endl;
+		}
+
+	}
+
+
+	// 연산이 끝날때까지 기다리면 안된다.
 	NewThread0.join();
 	NewThread1.join();
 
-
-	std::cout << Value;
-    // std::cout << Value0 + Value1;
 }
---
+++ CPlusPlus/102_ThreadEx/102_ThreadEx.cpp
@@ -0,0 +1,216 @@
+﻿// 102_ThreadEx.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <vector>
+#include <conio.h>
+#include <functional>
+
+// 이건 쓰레드마다 생기는 전역 변수가 됩니다.
+// 17부터 사용이 가능하다.
+thread_local int Value = 0;
+int ResultValue = 0;
+
+std::mutex Lock;
+
+HANDLE IOCPHandle = 0;
+
+class Job
+{
+public:
+	std::function<void()> Function;
+};
+
+
+enum class ThreadWork
+{
+	Job,
+	Destroy,
+};
+
+
+std::atomic_int ThreadIndex = 0;
+
+void Test() 
+{
+	std::wstring Text = L"IOCPWorkThread ";
+
+	Text += std::to_wstring(++ThreadIndex);
+
+	SetThreadDescription(GetCurrentThread(), Text.c_str());
+
+	DWORD Byte;
+	ULONG_PTR PtrKey;
+
+	// 서버할때만 의미가 있다.
+	LPOVERLAPPED OverLapped = nullptr;
+
+
+	while (true)
+	{
+		// 윈도우가 강제로 이 쓰레드를 정지시켜준것.
+		// GetQueuedCompletionStatusEx <= 이것은 좀더 상위
+		// 30개 쓰레드를 만들었다고 칠께요. 1000 텍스처 로딩
+		// 그런데. 
+
+		// 윈도우는 iocp를 지원해주고.
+		// 윈도우는 RIO라는 것까지 지원하고 
+		
+		// 리눅스는 epoll을 지원합니다.
+
+		GetQueuedCompletionStatus(IOCPHandle, &Byte, &PtrKey, &OverLapped, INFINITE);
+
+		ThreadWork WorkType = static_cast<ThreadWork>(Byte);
+
+		if (WorkType == ThreadWork::Destroy)
+		{
+			break;
+		}
+
+		if (WorkType == ThreadWork::Job)
+		{
+			 Job* JobPtr = reinterpret_cast<Job*>(PtrKey);
+
+			 if (nullptr != JobPtr->Function)
+			 {
+				 JobPtr->Function();
+			 }
+
+			 delete JobPtr;
+		}
+
+		// 일을 처리하는 로직을 만들겁니다.
+		// 일처리끝.
+
+		// 여기로 내려올때가 있어야 합니다.
+		// 우리가 쓰레드에게 
+		int a = 0;
+
+		std::wcout << Text << " to Work" << std::endl;
+
+		// 특정 쓰레드나 프로세스를 강제로 n초간 Cpu에 요청을 아무것도 안하게 만드는 함수가 
+
+		// 강제 컨텍스트 
+		// 단순히 슬립을 하나 걸어놓는것 만으로
+		// 비지쓰레드 문제를 해결할수 있다.
+		// busy thread
+		// Sleep(1);
+
+		// 진짜 일이 있으면 깨어나게 만드는 것이다.
+		// 윈도우는 이걸 하기위해서 Iocp라고 하는 쓰레드 핸들링 인터페이스를 지원합니다.
+		
+		// 일이 없는거야.
+
+		// 멈춘다 => 일이 생길때까지
+
+	}
+
+	//for (size_t i = 0; i < 100000; i++)
+	//{
+	//	++Value;
+	//}
+
+	//// 이게 락을 최소한으로 사용하는 방법중 하나 입니다.
+	//Lock.lock();
+	//ResultValue += Value;
+	//Lock.unlock();
+}
+
+void TextureLoading() 
+{
+	// aaa.png
+	// bbb.png
+	// ccc.png
+	std::cout << "텍스처를 로딩합니다" << std::endl;
+}
+
+void SoundLoading()
+{
+	std::cout << "사운드를 로딩합니다" << std::endl;
+}
+
+int main()
+{
+	// CreateIoCompletionPort는 2가지 용도로 사용되는데 윈도우에게 IOCP기능을 이용하겠다고 하고
+	// IOCP핸드를 받는 용도가 첫번째.
+	// 그 개수만큼의 쓰레드 관리를 하겠다고 하는것.
+	// 2번째 용도는 특정 통신회전을 담당하는 핸들을 관리해달라? => 서버할때
+	// 2번째 용도는 우리에게 의미가 없다.
+
+	IOCPHandle = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 30);
+
+	if (nullptr == IOCPHandle)
+	{
+		int a = 0;
+		return 1;
+	}
+
+
+	std::vector<std::thread> AllThread;
+	for (size_t i = 0; i < 30; i++)
+	{
+		AllThread.push_back(std::thread(Test));
+	}
+
+
+	
+	while (true)
+	{
+
+		int Select = _getch();
+
+		// 날아온 패킷의 크기
+		// 아무숫자나 넣어줘도 상관 없다.
+		// 4바이트 정수와
+		// 8바이트 정수를 쓰레드에게 안전하게 
+		// 포인터 => 8바이트 정수니까.
+
+		// 서버쪽과 파일입출력 쪽에서 사용된다.
+		// 파일을 n바이트까지 읽었습니다.
+
+
+		for (size_t i = 0; i < 10; i++)
+		{
+			Job* NewJob = new Job();
+
+			switch (Select)
+			{
+			case 'a':
+			case 'A':
+				NewJob->Function = TextureLoading;
+				break;
+			case 's':
+			case 'S':
+				NewJob->Function = SoundLoading;
+				break;
+			default:
+				break;
+			}
+
+
+			if (FALSE == PostQueuedCompletionStatus(IOCPHandle, static_cast<DWORD>(ThreadWork::Job), reinterpret_cast<unsigned __int64>(NewJob), nullptr))
+			{
+				int a = 0;
+			}
+		}
+
+	}
+
+	// 쓰레드를 만드는것 자체가 비용이 크기 때문에
+	// 필요할때마다 만드는 방법보다는
+	// 미리 많이 만들어 놓고
+	// 필요할때 깨우는 방법을 선호합니다.
+	// 쓰레드 풀이라고 합니다.
+
+	// std::cout << ResultValue << std::endl;
+
+	for (size_t i = 0; i < AllThread.size(); i++)
+	{
+		AllThread[i].join();
+	}
+}
