ae118b9323ecbd6ee9482e82ec42611a5c57f47c
chcjsdud <chcjsdud@naver.com>
2023-06-29 18:04:10 +0900

    조해근 쓰레드 추가

--- DirectX/DirectX45/GameEngineContents/TileMapLevel.cpp
+++ DirectX/DirectX45/GameEngineContents/TileMapLevel.cpp
@@ -134,10 +134,10 @@ void TileMapLevel::Start()
 		}
 	}
 
-	GetLastTarget()->CreateEffect<OldFilm>();
+	// GetLastTarget()->CreateEffect<OldFilm>();
 
 	// 가장 쉬운 방법이 스몰버퍼
-	GetLastTarget()->CreateEffect<BlurEffect>();
+	// GetLastTarget()->CreateEffect<BlurEffect>();
 }
 
 std::atomic AllLoadCount = 0;
