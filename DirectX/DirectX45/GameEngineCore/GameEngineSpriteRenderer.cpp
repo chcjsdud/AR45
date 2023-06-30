609671ba78c55a70b79cd0699f6ba140df4b7981
pasongsj <audrbekf@gmail.com>
2023-06-30 15:59:59 +0900

    이수정 : StartEventFunction Reset 추가

--- DirectX/DirectX45/GameEngineCore/GameEngineSpriteRenderer.cpp
+++ DirectX/DirectX45/GameEngineCore/GameEngineSpriteRenderer.cpp
@@ -19,6 +19,10 @@ void AnimationInfo::Reset()
 	CurTime = FrameTime[0];
 	IsEndValue = false;
 	IsPauseValue = false;
+	for (std::pair<const size_t, AnimationStartEvent>& Pair : StartEventFunction)
+	{
+		Pair.second.IsEvent = false;
+	}
 }
 
 void AnimationInfo::Update(float _DeltaTime)
