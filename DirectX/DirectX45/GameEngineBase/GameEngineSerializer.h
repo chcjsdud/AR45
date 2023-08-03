a7015ff7cdac8425b14dfaa9e6b589b978989fc5
chcjsdud 조해근 <chcjsdud@naver.com>
2023-08-04 08:19:43 +0900

    조해근 : 테스트 업데이트

--- DirectX/DirectX45/GameEngineBase/GameEngineSerializer.h
+++ DirectX/DirectX45/GameEngineBase/GameEngineSerializer.h
@@ -8,64 +8,123 @@
 // 포인터같은 저장해봐야 쓸모없는 데이터들을 다 제외하고 필요한 데이터만
 // 바이트 단위로 변경하는것을 의미합니다.
 
+
 // 설명 :
 class GameEngineSerializer
 {
 public:
 	// constrcuter destructer
 	GameEngineSerializer();
+	GameEngineSerializer(const char* _Data, unsigned int _Size);
 	~GameEngineSerializer();
 
 	// delete Function
-	GameEngineSerializer(const GameEngineSerializer& _Other) = delete;
-	GameEngineSerializer(GameEngineSerializer&& _Other) noexcept = delete;
-	GameEngineSerializer& operator=(const GameEngineSerializer& _Other) = delete;
-	GameEngineSerializer& operator=(GameEngineSerializer&& _Other) noexcept = delete;
-
-	void BufferResize(size_t _Size);
-
-	// 실수할 여지가 있어서 선생님은 템플릿을 나중에 만들려고 합니다.
-	void Write(const int& _Value);
-
-	void Write(const std::string_view& _Value);
-
-	void Write(const void* _Ptr, size_t _Size);
+	//GameEngineSerializer(const GameEngineSerializer& _Other) = delete;
+	//GameEngineSerializer(GameEngineSerializer&& _Other) noexcept = delete;
+	//GameEngineSerializer& operator=(const GameEngineSerializer& _Other) = delete;
+	//GameEngineSerializer& operator=(GameEngineSerializer&& _Other) noexcept = delete;
 
 
-	////////////////////// Read
-	void Read(int& _Value);
-
-	void Read(std::string& _Value);
-
-	void Read(void* _Ptr, size_t _Size);
-
-
-	inline void* GetData()
+	unsigned int GetReadOffSet()
 	{
-		return &Datas[0];
+		return ReadOffset;
 	}
 
-	inline const void* GetConstData() const
+	void ReadOffsetReset()
 	{
-		return &Datas[0];
+		ReadOffset = 0;
 	}
 
-	inline size_t GetOffset() const
+	unsigned int GetWriteOffSet()
 	{
-		return Offset;
+		return WriteOffset;
 	}
 
-	inline size_t GetBufferSize() const
+	void WriteOffsetReset()
 	{
-		return Datas.size();
+		WriteOffset = 0;
 	}
 
-	std::string GetString();
+	void Reset()
+	{
+		WriteOffset = 0;
+		ReadOffset = 0;
+	}
+
+	const std::vector<unsigned char>& GetData() {
+		return Data;
+	}
+
+	unsigned char* GetDataPtr() {
+		return &Data[0];
+	}
+
+	const char* GetDataConstPtr() {
+		return reinterpret_cast<const char*>(&Data[0]);
+	}
+
+
+	template<typename Type>
+	Type GetDataPtrConvert() {
+		return reinterpret_cast<Type>(&Data[0]);
+	}
+
+	void Write(const void* Data, unsigned int _Size);
+
+	void Read(void* Data, unsigned int _Size);
+
+	void ClearReadData();
+
+	void operator<<(const std::string& _Value);
+	void operator<<(const int _Value);
+	void operator<<(const unsigned int _Value);
+	void operator<<(const uint64_t& _Value);
+	void operator<<(const float _Value);
+
+	template<typename T>
+	void WriteEnum(const T _Value)
+	{
+		Write(reinterpret_cast<const void*>(&_Value), static_cast<unsigned int>(sizeof(T)));
+	}
+
+	template<typename T>
+	void WriteUserData(const T _Value)
+	{
+		Write(reinterpret_cast<const void*>(&_Value), static_cast<unsigned int>(sizeof(T)));
+	}
+
+
+	void operator>>(std::string& _Value);
+	void operator>>(int& _Value);
+	void operator>>(unsigned int& _Value);
+	void operator>>(uint64_t& _Value);
+	void operator>>(float& _Value);
+
+	template<typename T>
+	void ReadEnum(T& _Value)
+	{
+		Read(reinterpret_cast<void*>(&_Value), static_cast<unsigned int>(sizeof(T)));
+	}
+
+	template<typename T>
+	void ReadUserData(T& _Value)
+	{
+		Read(reinterpret_cast<void*>(&_Value), static_cast<unsigned int>(sizeof(T)));
+	}
 
 protected:
 
 private:
-	std::vector<char> Datas = std::vector<char>(1024);
-	size_t Offset = 0;
+	unsigned int WriteOffset = 0;
+	unsigned int ReadOffset = 0;
+	std::vector<unsigned char> Data;
+
 };
 
+
+class GameEngineSerializObject
+{
+public:
+	virtual void Write(GameEngineSerializer& _File) = 0;
+	virtual void Read(GameEngineSerializer& _File) = 0;
+};
